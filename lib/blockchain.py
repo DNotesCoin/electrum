b18580dc72da71ede54678a8103c7e6e479bf6ec
Nathan Soule <nathan.soule@geneca.com>
2018-05-04 16:47:03 -0500

    Update block validation for Pos

--- lib/blockchain.py
+++ lib/blockchain.py
@@ -159,11 +159,14 @@ class Blockchain(util.PrintError):
             raise Exception("prev hash mismatch: %s vs %s" % (prev_hash, header.get('prev_block_hash')))
         if constants.net.TESTNET:
             return
-        bits = self.target_to_bits(target)
-        if bits != header.get('bits'):
-            raise Exception("bits mismatch: %s vs %s" % (bits, header.get('bits')))
-        if int('0x' + _hash, 16) > target:
-            raise Exception("insufficient proof of work: %s vs target %s" % (int('0x' + _hash, 16), target))
+        #DNotes - assume local daemon is trusted and we don't need to recheck proof of stake, proof
+        #   of work checking from bitcoin here will be incorrect
+        #bits = self.target_to_bits(target)
+        #if bits != header.get('bits'):
+            #raise Exception("bits mismatch: %s vs %s" % (bits, header.get('bits')))
+        #if int('0x' + _hash, 16) > target:
+            #raise Exception("insufficient proof of work: %s vs target %s" % (int('0x' + _hash, 16), target))
+        return
 
     def verify_chunk(self, index, data):
         num = len(data) // 80
